5f05cc1db53263a8aa314ef5748e4f5d3360c6aa
sarasstha5 <sarasstha5@gmail.com>
2026-05-26 21:26:24 +0545

    Fix  create table
    Updated insert statement to correct syntax for inserting into the student table.

--- basic/create_Table.sql
+++ basic/create_Table.sql
@@ -1,8 +1,2 @@
 
-insert into student(roll_no, Fname,lname,address,email) values
-(1,'Saras','Shrestha','ratuwamai','saras324@gmail.com'),
-(2,'Hari','basnet','urlabari','hari232@gmail.com'),
-(3,'gita','kumari','damak','gita24@gmail.com'),
-(4,'Sita','Shrestha','ratuwamai','sita24@gmail.com'),
-(5,'babita','pradhan','urlabari','babita324@gmail.com');
-
+insert into table student(roll_no, Fname,lname,address,email);
