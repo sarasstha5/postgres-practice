e51074d542187717fed11cbe9cf41b5dfbf7ffe1
sarasstha5 <sarasstha5@gmail.com>
2026-05-26 19:37:49 +0545

    Add files via upload

---
+++ insert_into_table.sql
@@ -0,0 +1,8 @@
+
+insert into student(roll_no, Fname,lname,address,email) values
+(1,'Saras','Shrestha','ratuwamai','saras324@gmail.com'),
+(2,'Hari','basnet','urlabari','hari232@gmail.com'),
+(3,'gita','kumari','damak','gita24@gmail.com'),
+(4,'Sita','Shrestha','ratuwamai','sita24@gmail.com'),
+(5,'babita','pradhan','urlabari','babita324@gmail.com');
+
