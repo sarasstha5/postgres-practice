806f10c6aa63307b7fd966e53f40173142fe41e8
saras shrestha <sarasstha5@gmail.com>
2026-05-30 22:58:50 +0545

    Implement aggregate functions
    Add aggregate functions to retrieve statistics from detail table

---
+++ aggregate function/aggregate.sql
@@ -0,0 +1,8 @@
+select COUNT(id) from detail ;
+select avg(salary) from detail;
+select SUM(salary) from detail;
+select MIN(dept) from detail;
+select MAX(dept)from detail;
+
+-- use only one query at a time
+--aggregate function cannot be written in WHERE clause
