be0e116ccdd38b1bc21d11d44a5abaec9aa9634f
saras <sarasstha15@gmail.com>
2026-05-31 00:23:54 +0545

    added more exercise

--- string function/exercise.sql
+++ string function/exercise.sql
@@ -4,3 +4,12 @@ select concat_ws(':',id,fname,lname,country) from student;
 
 --2. make likee this 1:raj sharma:nepal
 select CONCAT_WS(':',id,CONCAT_WS(' ',fname,lname),county) from student;
+
+--3. saras:RATUWA
+select CONCAT_WS(':',fname,UPPER(address))from student
+where id = 1;
+
+--4. N1 saras
+     U8 john
+select CONCAT_WS('',substr(county,1,1),CONCAT_WS(' ',id,fname)) from student
+where id = 1 OR id = 8;
