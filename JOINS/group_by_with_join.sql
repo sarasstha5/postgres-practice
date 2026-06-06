02a07bc3508e5c5087f029791ac78cf48f37f6d3
saras shrestha <sarasstha5@gmail.com>
2026-06-07 01:29:38 +0545

    Enhance SQL with additional course counting query
    Added a second query to count total courses per student with course details.

--- JOINS/group_by_with_join.sql
+++ JOINS/group_by_with_join.sql
@@ -1,10 +1,22 @@
 --students(id,name)
 --enrollment(enrollment_id,s_id,c_id,enrollment_date) 
 --courses(c_id,name,fee)
-SELECT
+"SELECT
     s.name,
     COUNT(e.c_id) AS total_courses
 FROM students s
 JOIN enrollment e
     ON s.s_id = e.s_id
 GROUP BY s.name;
+    "
+
+SELECT
+    s.student_name,
+    COUNT(c.c_id) AS total_courses
+FROM enrollment e
+JOIN students s
+    ON e.s_id = s.s_id
+JOIN courses c
+    ON e.c_id = c.c_id
+GROUP BY s.student_name;
+
